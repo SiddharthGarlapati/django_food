e9bb7e2951cab36d913b7e9da02ff040ade5daff
Siddharth Garlapati <siddu200095@gmail.com>
2022-03-14 11:22:36 +0530

    14march

--- base/models.py
+++ base/models.py
@@ -3,20 +3,20 @@ from django.db import models
 # Create your models here.
 
 class kid(models.Model):
-      kid_name = models.CharField(max_length = 50)
-      kid_age = models.IntegerField(max_length = 3)
-      parent_ph_number = models.CharField(max_length = 12)
-      parent_email_address = models.IntegerField(max_length = 100)
+      kid_name = models.CharField(max_length=50)
+      kid_age = models.IntegerField()
+      parent_ph_number = models.CharField(max_length=12)
+      parent_email_address = models.CharField(max_length=100)
 
 class image(models.Model):
 
     food_choices = (('veg','veg'),('fruit','fruit'),('Grain','Grain'),('Protein','Protein'),('Dairy','Dairy'),('Confectionery','Confectionery'),('Unknown','Unknown'))
     kid = models.ForeignKey(kid,on_delete = models.CASCADE)
-    image_url = models.URLField(max_length = 200 )
+    image_url = models.URLField(max_length=200)
     created_on = models.DateTimeField(auto_now_add = True)
     updated_on = models.DateTimeField(auto_now = True)
     is_approved = models.BooleanField()
-    food_group = models.CharField(max_length = 100, choices = food_choices)
+    food_group = models.CharField(max_length=100, choices = food_choices)
 
 
 
